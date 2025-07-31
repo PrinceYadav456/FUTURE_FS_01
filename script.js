84a4ccf21a208cc6698ce975efa8426058cf80c5
PrinceYadav456 <princeyaday456@gmail.com>
2025-07-31 13:30:46 +0530

    finished portfolio

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function toggleMenu(){
+function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-   const icon = document.querySelector(".hamburger-icon");
-   menu.classList.toggle("open");
-   icon.classList.toggle("open");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+(function () {
+  emailjs.init("k4nMtUV2vK6-vn7di"); // Replace with your EmailJS public key
+})();
+
+// Form submission
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_e69ukv9", "template_hroaall", this).then(
+      function () {
+        document.getElementById("status-message").innerText =
+          "Message sent successfully!";
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        document.getElementById("status-message").innerText =
+          "Failed to send message. Try again!";
+        console.log(error);
+      }
+    );
+  });
